40c054494ab41dbb202caf10d0b2013d944233b1
Charlie <lucharles3@gmail.com>
2020-12-01 19:05:59 -0500

    Update FooterNav.js
    got rid of unneeded links and set correct link paths

--- src/components/layout/partials/FooterNav.js
+++ src/components/layout/partials/FooterNav.js
@@ -19,16 +19,10 @@ const FooterNav = ({
     >
       <ul className="list-reset">
         <li>
-          <Link to="#0">Contact</Link>
+          <Link to="/about">About us</Link>
         </li>
         <li>
-          <Link to="#0">About us</Link>
-        </li>
-        <li>
-          <Link to="#0">FAQ's</Link>
-        </li>
-        <li>
-          <Link to="#0">Support</Link>
+          <Link to="/reviews">Reviews</Link>
         </li>
       </ul>
     </nav>
