656fdc5ebea89f83ea3cbb8555c93aff7641b202
Charlie <lucharles3@gmail.com>
2020-12-02 18:14:55 -0500

    reviews title
    best cryptocurrency exchanges

--- src/views/Reviews.js
+++ src/views/Reviews.js
@@ -1,27 +1,14 @@
 import React from 'react';
 
+// sections
+import Hero from '../components/sections/pages/reviews/Hero';
+
 const Reviews = () => {
 
-    return ( 
-        <section className='hero section illustration-section-01'>
-            <div className='container-sm hero-inner section-inner'>
-                <div className='site-content center-content '>
-                <div className="hero-content">
-
-                    <h1 className="mt-0 mb-16 reveal-from-bottom" data-reveal-delay="200">
-                        <span className="text-color-secondary">Reviews</span>
-                    </h1>
-
-                    <div className="container-xs">
-                        <p className="m-0 mb-32 reveal-from-bottom" data-reveal-delay="400">
-                            this is where the reviews page content will be
-                        </p>
-                    </div>
-
-                </div>
-                </div>
-            </div>
-        </section>
+    return (
+    <>
+        <Hero bottomDivider className='illustration-section-01'/>   
+    </>
     )
 }
 
