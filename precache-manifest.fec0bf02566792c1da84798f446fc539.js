3dd261e7d72d49565c342299f03b8d78e504ebce
Charlie <lucharles3@gmail.com>
2020-12-04 21:20:04 -0500

    Updates

---
+++ precache-manifest.fec0bf02566792c1da84798f446fc539.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40b70e6966f0b8769888f66d9e5e3886",
+    "url": "/crypto-block/index.html"
+  },
+  {
+    "revision": "d637d81067071736f27d",
+    "url": "/crypto-block/static/css/main.cb8f65a7.chunk.css"
+  },
+  {
+    "revision": "8da5ac40013473593507",
+    "url": "/crypto-block/static/js/2.ba9c07e9.chunk.js"
+  },
+  {
+    "revision": "ce15a0ffcfe8b91e4f219c1d7bb8c5ef",
+    "url": "/crypto-block/static/js/2.ba9c07e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d637d81067071736f27d",
+    "url": "/crypto-block/static/js/main.5c33eac9.chunk.js"
+  },
+  {
+    "revision": "92c23ca2d179d60d3db8",
+    "url": "/crypto-block/static/js/runtime-main.a9bcbbd3.js"
+  },
+  {
+    "revision": "3ccc032ee507d0c45d32ffdf9bb2515b",
+    "url": "/crypto-block/static/media/cryptocom-logo.3ccc032e.jpg"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/crypto-block/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "0f9928d77bc67692e90527ec092fcbd0",
+    "url": "/crypto-block/static/media/feature-tile-icon-01.0f9928d7.svg"
+  },
+  {
+    "revision": "4e5db7d436281b7150a58924c32b3b6b",
+    "url": "/crypto-block/static/media/features-split-image-01.4e5db7d4.png"
+  },
+  {
+    "revision": "e617c230fe11adca45b7381b74c0d6a5",
+    "url": "/crypto-block/static/media/features-split-image-01.e617c230.png"
+  },
+  {
+    "revision": "bcde8e70866e9cfa64f8ce05c70d4344",
+    "url": "/crypto-block/static/media/features-split-image-02.bcde8e70.png"
+  },
+  {
+    "revision": "ea6949039d6554ebf2cf109f333f5811",
+    "url": "/crypto-block/static/media/illustration-section-01.ea694903.svg"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "/crypto-block/static/media/illustration-section-02.63d0555d.svg"
+  },
+  {
+    "revision": "ee235c7177fc5f5af201aaaf92296ae8",
+    "url": "/crypto-block/static/media/logo.ee235c71.svg"
+  }
+]);
