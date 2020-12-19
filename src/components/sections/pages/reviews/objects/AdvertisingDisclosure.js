a6718989fea934f38200899cf6f6146b9576193f
Charlie <lucharles3@gmail.com>
2020-12-19 16:06:33 -0500

    text html

--- src/components/sections/pages/reviews/objects/AdvertisingDisclosure.js
+++ src/components/sections/pages/reviews/objects/AdvertisingDisclosure.js
@@ -7,17 +7,17 @@ const advertisingModalTrigger = (
   );
 
   const advertisingDiclosureText = (
-    <p className='text-xs'>
-      We make money through affiliate links. Here is how they generally work: 
-      <br/><br/>
-      If you click on one of the “Learn More” buttons and sign up for the product/service within a specified time frame, we will receive a portion of the money you spend on that product/service. This is known as a revenue share model. 
-      <br/><br/>
-      <b>The more you enjoy and actively use the products/services we recommend, the more money we make!</b>
-      <br/><br/>
-      The specified time frame and amount of compensation we receive will vary depending on how these products/services structure their program.
-      <br/><br/>
-      Note that even though we are financially incentivised for you to spend money on their platform, affiliate programs <b>will never</b> influence how we structure content on our website. Because most companies will offer an affiliate program to attract new customers, these programs will not play a role in our decision making process. Although some of these affiliate programs may be more attractive than others from a compensation perspective, it will not impact which companies we choose to support and advertise. It simply does not align with our mission.
-    </p>
+    <span className='text-xs'>
+      <p>We make money through affiliate links. Here is how they generally work:</p>
+
+      <p>If you click on one of the “Learn More” buttons and sign up for the product/service within a specified time frame, we will receive a portion of the money you spend on that product/service. This is known as a revenue share model.</p>
+
+      <p><b>The more you enjoy and actively use the products/services we recommend, the more money we make!</b></p>
+
+      <p>The specified time frame and amount of compensation we receive will vary depending on how these products/services structure their program.</p>
+
+      <p>Note that even though we are financially incentivised for you to spend money on their platform, affiliate programs <b>will never</b> influence how we structure content on our website. Because most companies will offer an affiliate program to attract new customers, these programs will not play a role in our decision making process. Although some of these affiliate programs may be more attractive than others from a compensation perspective, it will not impact which companies we choose to support and advertise. It simply does not align with our mission.</p>
+    </span>
   );
 
   export {
--- src/views/secondary/Terms.js
+++ src/views/secondary/Terms.js
@@ -9,42 +9,34 @@ import GenericSection from '../../components/sections/GenericSection';
 const Terms = () => {
 
     const termsText = (
-        <p className='text-sm'> 
-            <u className='fw-600'>Introduction</u>
-            <br />
-                Crypto Block is an online educational website about Blockchain and Cryptocurrency. The following are the Terms of Service for the Crypto Block website.
-            <br /><br />
-            <u className='fw-600'>Information Provided</u>
-            <br />
-                All content supplied to the Crypto Block website is a combination of aggregated information and opinions of the writers. 
+        <span className='text-sm'> 
+            <p><u className='fw-600'>Introduction</u></p>
+                <p>Crypto Block is an online educational website about Blockchain and Cryptocurrency. The following are the Terms of Service for the Crypto Block website.</p>
+
+            <p><u className='fw-600'>Information Provided</u></p>
+                <p>All content supplied to the Crypto Block website is a combination of aggregated information and opinions of the writers. 
                 Although we will conduct due diligence on our research, we cannot 100% guarantee that all information provided will be timely or accurate. 
-                We recommend all users of the Crypto Block website to conduct outside research from a number of sources in order to seek out their desired content/knowledge.  
-            <br /><br />
-            <u className='fw-600'>Not Financial Advice</u>
-            <br />
-                Any information obtained from the Crypto Block website should not be treated as financial advice. 
-                Investment decisions made by users of the Crypto Block website will take full responsibility for their own actions.
-            <br /><br />
-            <u className='fw-600'>Privacy Policy</u>
-            <br />
-            As of right now, we do not collect any data from visitors of the Crypto Block website. We will update our privacy policy in the event that this changes.
-            <br /><br />
-            Any external links that redirect to websites outside of the Crypto Block domain will have their own separate Privacy Policy. These external websites may collect browser cookies data on top of any information you supply to the external website. We take no responsibility for any data collection that takes place outside of the Crypto Block domain. 
-            <br /><br />
-            Our affiliate software, <a href='https://impact.com/'><u>Impact</u></a>, does allow us to view data related to our affiliate links. Note that we do not collect this data, but rather the Impact software provides us information so that our affiliate partners can compensate us appropriately. This information is not personally identifiable, and Crypto Block only gets to see a broad overview of the numbers. The data provided includes: 
-            <br /><br />
-            <ul>
-                <li>
-                Number of clicks on the affiliate link.
-                </li>
-                <li>
-                Number of actions taken on our affiliate partner’s website. 
-                </li>
-                <li>
-                Amount of money spent on our affiliate partner’s website.
-                </li>
-            </ul>
-        </p>
+                We recommend all users of the Crypto Block website to conduct outside research from a number of sources in order to seek out their desired content/knowledge.</p> 
+
+            <p><u className='fw-600'>Not Financial Advice</u></p>
+                <p>Any information obtained from the Crypto Block website should not be treated as financial advice. 
+                Investment decisions made by users of the Crypto Block website will take full responsibility for their own actions.</p>
+
+            <p><u className='fw-600'>Privacy Policy</u></p>
+                <p>As of right now, we do not collect any data from visitors of the Crypto Block website. We will update our privacy policy in the event that this changes.</p>
+                <p>Our affiliate software, <a href='https://impact.com/'><u>Impact</u></a>, does allow us to view data related to our affiliate links. Note that we do not collect this data, but rather the Impact software provides us information so that our affiliate partners can compensate us appropriately. This information is not personally identifiable, and Crypto Block only gets to see a broad overview of the numbers. The data provided includes:</p>
+                <ul>
+                    <li>
+                    <p>Number of clicks on the affiliate link.</p>
+                    </li>
+                    <li>
+                    <p>Number of actions taken on our affiliate partner’s website.</p>
+                    </li>
+                    <li>
+                    <p>Amount of money spent on our affiliate partner’s website.</p>
+                    </li>
+                </ul>
+        </span>
     );
 
     return (
