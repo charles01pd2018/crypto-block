b6c07075b12a395be7c01f18126e0fc411e4928c
Charlie <lucharles3@gmail.com>
2021-02-01 20:48:17 -0500

    Update sitemap-generator.js

--- sitemap-generator.js
+++ sitemap-generator.js
@@ -9,18 +9,6 @@ require.extensions['.css'] = function () {
     return null;
   };
   
-  require.extensions['.png'] = function () {
-    return null;
-  };
-  
-  require.extensions['.svg'] = function () {
-    return null;
-  };
-  
-  require.extensions['.jpg'] = function () {
-    return null;
-  };
-
   const filterConfig = {
 	isValid: false,
 	rules: [
