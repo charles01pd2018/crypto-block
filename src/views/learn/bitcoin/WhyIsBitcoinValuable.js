7e5bc80e9c8bf3b556ab02eeb4e1631815817b6c
Charlie <lucharles3@gmail.com>
2021-02-03 17:02:12 -0500

    bitcoin banner page setup

--- src/views/learn/bitcoin/WhyIsBitcoinValuable.js
+++ src/views/learn/bitcoin/WhyIsBitcoinValuable.js
@@ -3,6 +3,8 @@ import { Helmet } from "react-helmet";
 
 // sections
 import { Hero, BitcoinStoreValue, BitcoinGlobalEconomy, BitcoinTokenomics } from '../../../components/sections/pages/learn/bitcoin/why-is-bitcoin-valuable';
+import { Banner } from '../../../components/elements';
+
 
 
 const WhyIsBitcoinValuable = () => {
@@ -14,6 +16,7 @@ const WhyIsBitcoinValuable = () => {
                 <title>Why is Bitcoin Valuable?</title>
             </Helmet>
 
+            <Banner />
             <Hero bottomDivider className='illustration-section-01'/>
             <BitcoinStoreValue invertMobile bottomDivider />
             <BitcoinGlobalEconomy invertMobile bottomDivider />
